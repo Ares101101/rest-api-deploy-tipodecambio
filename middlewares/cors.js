9263929eb76bc2ffc582b6fe1312f019bbe70183
diego <diego>
2025-08-12 11:27:06 -0500

    all

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,7 @@
 import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://localhost:3002',
-  'http://localhost:8080'
+  '*'
 ]
 
 export const corsmiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
